0ff7ff90f4f73e9bdaeba7ac48fcb18d6a7eb237
Abdus Samad <abdusjscript@gmail.com>
2024-08-05 05:46:48 +0600

    Profile info added

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,43 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { actions } from "../actions";
+import MyPost from "../components/profile/MyPost";
+import ProfileInfo from "../components/profile/ProfileInfo";
 import useAuth from "../hooks/useAuth";
 import useAxios from "../hooks/useAxios";
+import { useProfile } from "../hooks/useProfile";
 
 export default function ProfilePage() {
-  const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
+  const { state, dispatch } = useProfile();
   const { auth } = useAuth();
   const { api } = useAxios();
 
   useEffect(() => {
-    setLoading(true);
+    dispatch({ type: actions.profile.DATA_FETCHING });
     const fetchProfile = async () => {
       try {
         const response = await api.get(
           `${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id}`
         );
-        setUser(response?.data?.user);
-        setPosts(response?.data?.posts);
+        if (response.status === 200) {
+          dispatch({ type: actions.profile.DATA_FETCHED, data: response.data });
+        }
       } catch (error) {
-        confirm.error(error);
-        setError(error);
-      } finally {
-        setLoading(false);
+        console.error(error);
+        dispatch({
+          type: actions.profile.DATA_FETCH_ERROR,
+          error: error.message,
+        });
       }
     };
     fetchProfile();
   }, []);
 
-  if (loading) {
+  if (state?.loading) {
     return <div> Fetching your profile data...</div>;
   }
 
   return (
     <div>
-      Welcome, {user?.firstName} {user?.lastName}
-      <p>You have {posts.length} posts.</p>
+      <ProfileInfo />
+      <MyPost />
     </div>
   );
 }
