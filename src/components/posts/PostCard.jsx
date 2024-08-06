321dca0cef687db77bef7447dfa7815a1677e69e
Abdus Samad <abdusjscript@gmail.com>
2024-08-06 09:38:34 +0600

    like and comment functionality added

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import PostAction from "./PostAction";
 import PostBody from "./PostBody";
 import PostComment from "./PostComment";
 import PostHeader from "./PostHeader";
 
 export default function PostCard({ post }) {
+  const [commentCount, setCommentCount] = useState(post?.comments?.length);
+
+  const commentAdd = () => {
+    setCommentCount(commentCount + 1);
+  };
+
   return (
     <article className="card mt-6 lg:mt-8">
       {/* post header */}
@@ -11,9 +18,9 @@ export default function PostCard({ post }) {
       {/* post body */}
       <PostBody poster={post?.image} content={post?.content} />
       {/* post actions */}
-      <PostAction postID={post?.id} commentCount={post?.comments?.length} />
+      <PostAction post={post} commentCount={commentCount} />
       {/* comment section */}
-      <PostComment post={post} />
+      <PostComment post={post} commentAdd={commentAdd} />
     </article>
   );
 }
--- src/components/posts/PostComment.jsx
+++ src/components/posts/PostComment.jsx
@@ -1,15 +1,44 @@
-import { useAvatar } from "../../hooks/useAvatar";
+import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
 import PostCommentList from "./PostCommentList";
 
-export default function PostComment({ post }) {
-  const { avatarURL } = useAvatar(post);
+export default function PostComment({ post, commentAdd }) {
+  const [showComment, setShowComment] = useState(false);
+  const [comments, setComments] = useState(post?.comments);
+  const [comment, setComment] = useState("");
+  const { api } = useAxios();
+  const { auth } = useAuth();
+
+  const addComment = async (event) => {
+    const keyCode = event.keyCode;
+
+    if (keyCode === 13) {
+      try {
+        const response = await api.patch(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/comment`,
+          { comment }
+        );
+
+        if (response.status === 200) {
+          setComments([...response.data.comments]);
+          setShowComment(true);
+          setComment("");
+          commentAdd();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div>
       {/* comment input box */}
       <div className="flex-center mb-3 gap-2 lg:gap-4">
         <img
           className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-          src={avatarURL}
+          src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
           alt="avatar"
         />
         <div className="flex-1">
@@ -18,16 +47,24 @@ export default function PostComment({ post }) {
             className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
             name="post"
             id="post"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            onKeyDown={(e) => addComment(e)}
             placeholder="What's on your mind?"
           />
         </div>
       </div>
       {/* comment filter button */}
       <div className="mt-4">
-        <button className="text-gray-300 max-md:text-sm">All Comment ▾</button>
+        <button
+          className="text-gray-300 max-md:text-sm"
+          onClick={() => setShowComment(!showComment)}
+        >
+          All Comment ▾
+        </button>
       </div>
       {/* comments */}
-      <PostCommentList comments={post?.comments} />
+      {showComment && <PostCommentList comments={comments} />}
       {/* comments ends */}
     </div>
   );
